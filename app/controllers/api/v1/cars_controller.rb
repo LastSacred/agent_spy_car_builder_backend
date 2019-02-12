de1a776f4e6653cc85a816a339a814feac2713bd
Arthur Torres <arthurtorres002@gmail.com>
2019-02-11 20:36:22 -0500

    configure cors

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::CarsController < ApplicationController
 
   def show
     car = Car.find(params[:id])
-    render json: car.to_json(includ: [:powertrain, :exterior, :wheel, :interior, :accessory])
+    render json: car.to_json(include: [:powertrains, :exteriors, :wheels, :interiors, :accessories])
   end
 
 end
