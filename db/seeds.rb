e0bbe24d8c9e40dec93b3ef37302c9106c89badd
Arthur Torres <arthurtorres002@gmail.com>
2019-02-13 11:34:54 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,18 @@
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at porttitor dui. Duis fermentum ipsum et velit mollis ullamcorper. Vestibulum."
 
 p1 = Powertrain.create(name: "Dr. No", drivetrain: "Rear Two Wheel Drive", engine: "V6", standard: true, msrp: 0.00)
+p5 = Powertrain.create(name: "RATT-GX", drivetrain: "All Wheel Drive", engine: "Grail Engine", standard: true, msrp: 11799.00)
 p2 = Powertrain.create(name: "Oddjob", drivetrain: "Rear Two Wheel Drive", engine: "Electric Hybrid", standard: false, msrp: 3400.00)
 p3 = Powertrain.create(name: "Le Chiffre", drivetrain: "All Wheel Drive", engine: "V8 Super Charged", standard: false, msrp: 7800.00)
 p4 = Powertrain.create(name: "Jaws", drivetrain: "All Wheel Drive", engine: "V12", standard: false, msrp: 14500.00)
-p5 = Powertrain.create(name: "RATT-GX", drivetrain: "All Wheel Drive", engine: "Grail Engine", standard: true, msrp: 11799.00)
 
 e1 = Exterior.create(color: "Skyfall Grey", trim: "Polymer Treated Plastic", standard: true , msrp: 0.00)
+e7 = Exterior.create(color: "Quantum of Solace Black", trim: "Euro Style Trim", standard: true, msrp: 900.00)
 e2 = Exterior.create(color: "Casino Royal Red", trim: "Triple Dipped Chrome", standard: false, msrp: 560.00)
 e3 = Exterior.create(color: "Moonraker White", trim: "Euro Style Trim", standard: false, msrp: 800.00)
 e4 = Exterior.create(color: "Octopussy Purple", trim: "Thermal Heat Blocking", standard: false, msrp: 34900.00)
 e5 = Exterior.create(color: "GoldenEye Gold", trim: "Fire Proof", standard: false, msrp: 24000.00)
 e6 = Exterior.create(color: "Quantum of Solace Black", trim: "Invisibility Cloak", standard: false, msrp: 139000.00)
-e7 = Exterior.create(color: "Quantum of Solace Black", trim: "Euro Style Trim", standard: true, msrp: 900.00)
 
 w1 = Wheel.create(name: "BBS Style 5's", size: "17x8.5", finish: "Gloss Silver", standard: true, msrp: 0.00)
 w2 = Wheel.create(name: "Mono Bloc Racing Wheels - Por 991 RSR", size: "18x10.5", finish: "Powder Coated White", standard: false, msrp: 1899.00)
@@ -28,10 +28,10 @@ w3 = Wheel.create(name: "Super RS - 2 Piece - Tire Slashing HubCaps", size: "19x
 w4 = Wheel.create(name: "BBS GT3 19 - Hubcap Lazers", size: "19x11.5", finish: "Gloss Black", standard: false, msrp: 15900.00)
 
 i1 = Interior.create(upholstery: "Nappa Leather - Contrast Stitching & Piping", color: "Black", standard: true, trim: "Aluminum Rhombicle Interior Trim", msrp: 0.00)
+i5 = Interior.create(upholstery: "Full Merino Leather", color: "Black", standard: false, trim: "Aluminum Rhombicle Interior Trim", msrp: 4400.00)
 i2 = Interior.create(upholstery: "Nappa Leather - Contrast Stitching & Piping", color: "Ivory White", standard: false, trim: "Burl Walnut Wood Trim", msrp: 1200.00)
 i3 = Interior.create(upholstery: 'Full Merino Leather', color: "Caramel-Black", standard: false, trim: "Gray Poplar Wood Trim", msrp: 3099.00)
 i4 = Interior.create(upholstery: "Full Merino Leather", color: "Tartufo", standard: false, trim: "Plum Brown Wood Trim", msrp: 4200.00)
-i5 = Interior.create(upholstery: "Full Merino Leather", color: "Black", standard: false, trim: "Aluminum Rhombicle Interior Trim", msrp: 4400.00)
 i6 = Interior.create(upholstery: "Full Merino Leather", color: "Ivory White", standard: false, trim: "Plum Brown Wood Trim", msrp: 4700.00)
 
 a1 = Accessory.create(name: "Triple Pained Bullet Proof Glass", description: lorem, msrp: 6999.00)
